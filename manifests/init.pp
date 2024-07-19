fef18fb68516b446ed4fac05ca76b27069ad9ece
root <root@ubuntu-srv3.myguest.virtualbox.org>
2024-07-19 18:13:30 +0300

    Working on puppet relations in classes site.pp

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+## site.pp file for manifests
+##
+##
+##
+
+class ssh(
+  String $package_name = $::ssh::params::package_name,
+  String $service_name = $::ssh::params::service_name,
+) inherits ::ssh::params {
+  class { '::ssh::install': }
+  class { '::ssh::service':
+    require => Class['::ssh::install']
+  }
+}
