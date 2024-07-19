725419f56a346379f411ffc01b76409cf0fab58d
root <root@ubuntu-srv3.myguest.virtualbox.org>
2024-07-19 19:00:26 +0300

    params.pp 'unless' statement usage

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,20 @@
 class ssh::params {
-  if $facts['os']['family'] == 'Debian' {
+  unless $facts['os']['family'] == 'Debian' {
     $package_name = 'openssh-server'
-    $service_name = 'ssh'
+    $service_name = 'sshd'    
   }
-  elsif $facts['os']['family'] == 'Redhat' {
-    $package_name = 'openssh-server'
-    $service_name = 'sshd'
-  }
-  else {
-    fail("${facts['operatingsystem']} is not supported!")
-  }
+
+#  if $facts['os']['family'] == 'Debian' {
+#    $package_name = 'openssh-server'
+#    $service_name = 'ssh'
+#  }
+#  elsif $facts['os']['family'] == 'Redhat' {
+#    $package_name = 'openssh-server'
+#    $service_name = 'sshd'
+#  }
+#  else {
+#    fail("${facts['operatingsystem']} is not supported!")
+#  }
 
 #  case $facts['os']['family'] {
 #    'Debian': {
