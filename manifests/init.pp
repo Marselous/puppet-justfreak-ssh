5e7eedebc522e8e3e062c6f9d39e22e7adf342ce
root <root@ubuntu-srv3.myguest.virtualbox.org>
2024-07-19 22:51:43 +0300

    config.pp as a configuration for our ssh

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,11 @@ class ssh(
   String $service_name = $::ssh::params::service_name,
 ) inherits ::ssh::params {
   class { '::ssh::install': }
-  class { '::ssh::service':
-    require => Class['::ssh::install']
+  class { 'ssh::config': }
+  class { '::ssh::service': }
+  
+  Class['::ssh::install']
+  -> Class['::ssh::config']
+  ~> Class['::ssh::service']
+  -> Class['ssh']
   }
-}
