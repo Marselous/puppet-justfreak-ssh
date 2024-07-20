f6f04f359e0a8fb7784357f1fa0bd43d6a591aff
root <root@ubuntu-srv3.myguest.virtualbox.org>
2024-07-20 19:52:04 +0300

    testing top level scope and node level scope

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,17 +4,32 @@
 ##
 
 class ssh(
-  Boolean $permit_root_login = $::ssh::params::permit_root_login,
-  Integer $port              = $::ssh::params::port,
-  String $package_name       = $::ssh::params::package_name,
-  String $service_name       = $::ssh::params::service_name,
-) inherits ::ssh::params {
+ # Boolean $permit_root_login = $::ssh::params::permit_root_login,
+ # Integer $port              = $::ssh::params::port,
+ # String $package_name       = $::ssh::params::package_name,
+ # String $service_name       = $::ssh::params::service_name,
+#) inherits ::ssh::params {
+  String $package_name,
+  String $service_name,
+  String $ensure,
+  String $service_ensure,
+  Boolean $service_enable,
+  Boolean $permit_root_login = false,
+  Integer $port              = 22,
+) {
+  notify { "module: test top scope var in a module": 
+    message => "test top scope ${::test_top_scope}"
+  }
+## This will not work:
+#  notify { "module: test node scope var in a module":
+#    message => "test top scope ${::node_scope_var}"
+#  }
   class { '::ssh::install': }
-  class { 'ssh::config': }
+  class { '::ssh::config': }
   class { '::ssh::service': }
   
   Class['::ssh::install']
   -> Class['::ssh::config']
   ~> Class['::ssh::service']
   -> Class['ssh']
-  }
+}
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,10 @@
 class ssh::install(
   String $package_name = $::ssh::package_name,
+  String $ensure       = $::ssh::ensure,
 ) {
   package { 'ssh-package':
-    ensure => present,
+   # ensure => present,
+    ensure => $ensure,
     name   => $package_name,
   }
 }
